f3441768d462c7aa3204fd844c27b58bfdc21e19
Kenn1314 <hojikenn1@gmail.com>
2023-03-31 16:11:29 +0800

    home_new

---
+++ resources/views/home_Test_1.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="d-flex flex-column">
+            <div class="row">
+                <div class="col-12 mb-4">
+                    <div class="row">
+                        <div class="col-xxl-4 col-xl-4 col-md-6 widget">
+                            <a href="https://infycare.infyom.com/patients/appointments" class="text-decoration-none">
+                                <div class="bg-primary rounded-10 p-xxl-10 px-7 py-10 d-flex align-items-center justify-content-between my-3">
+                                    <div class="bg-cyan-300 widget-icon rounded-10 d-flex align-items-center justify-content-center">
+                                       <svg> </svg>
+                                    </div>
+                                    <div class="text-end text-white">ss</div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xxl-4 col-xl-4 col-md-6 widget">s</div>
+                        <div class="col-xxl-4 col-xl-4 col-md-6 widget">s</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
