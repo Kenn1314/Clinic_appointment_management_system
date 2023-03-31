b2ebea3e4b538ca02a7eec225675a52a54a7e0d8
UtardaBest <alvinzzz@hotmail.com>
2023-03-31 14:00:41 +0800

    hiii

--- resources/views/patient/viewpatient.blade.php
+++ resources/views/patient/viewpatient.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class ="container-sm" >
-<h3>{{$name['name']}}</h3>
+{{-- <h3>{{$name['name']}}</h3> --}}
 <table class="table table-dark table-bordered border-2 border-secondary" >
     <thead>
         <td>Symptoms</td>
@@ -21,7 +21,7 @@
                             <td>{{ $details['updated_at'] }}</td>
                             <td>{{ $details['test_result'] }}</td>
                             <td>
-                                <a href="viewpatient/{{$name['id']}}">Edit</a>
+                                <a href="/patient/updatepatient/{{$details['id']}}">Edit</a>
                             </td> 
                         </tr>
     @endforeach
