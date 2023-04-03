838bc4c40e4a283bfd26d2aca7e6032ad0137c3e
Kenn1314 <hojikenn1@gmail.com>
2023-04-03 15:43:15 +0800

    newwwwwwww

--- resources/views/patient/patient_main.blade.php
+++ resources/views/patient/patient_main.blade.php
@@ -266,12 +266,9 @@
                                 <span>
 
                                     <!-- CANCEL BUTTON -->
-                                    <a data-bs-toggle="modal"
-                                        data-bs-target="#cancelModal_{{$upcoming_booking['id']}}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                            fill="red" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
-                                            <path
-                                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                                    <a data-bs-toggle="modal" data-bs-target="#cancelModal_{{$upcoming_booking['id']}}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
                                         </svg>
                                     </a>
 
@@ -281,44 +278,9 @@
                                     'aria_label' => 'cancelModalLabel',
                                     'modal_title' => 'Cancel Request',
                                     'modal_body' => 'DO YOU WANT TO CANCEL THIS APPOINTMENT ?',
-                                    'delete_id' => $upcoming_booking['id'],
+                                    'id' => $upcoming_booking['id'],
+                                    'route_name' => '/cancel/'.$upcoming_booking['id']
                                     ])
-
-                                    <!-- CANCEL MODAL -->
-                                    {{-- <div class="modal fade" id="cancelModal_1" tabindex="-1"
-                                        aria-labelledby="cancelModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-
-                                                <!-- MODAL HEADER -->
-                                                <div class="modal-header">
-                                                    <h1 class="modal-title fs-5" id="cancelModalLabel">
-                                                        Cancel Request
-                                                    </h1>
-                                                    <button type="button" class="btn-close"
-                                                        data-bs-dismiss="modal" aria-label="Close"></button>
-                                                </div>
-
-                                                <!-- MODAL BODY -->
-                                                <div class="modal-body">
-                                                    DO YOU WANT TO CANCEL THIS APPOINTMENT ?
-                                                    {{$upcoming_booking['id']}}
-                                                </div>
-
-                                                <!-- MODAL FOOTER -->
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary"
-                                                        data-bs-dismiss="modal">Close
-                                                        {{$upcoming_booking['id']}}</button>
-                                                    <button type="button" class="btn btn-danger">
-                                                        <a href="/cancel/{{$upcoming_booking['id']}}"
-                                                            style="color: white; text-decoration:none">Confirm</a>
-                                                    </button>
-                                                </div>
-
-                                            </div>
-                                        </div>
-                                    </div> --}}
                                 </span>
 
                             </div>
@@ -469,7 +431,8 @@
                                     'aria_label' => 'cancelModalLabel',
                                     'modal_title' => 'Cancel Request',
                                     'modal_body' => 'DO YOU WANT TO CANCEL THIS APPOINTMENT ?',
-                                    'delete_id' => $pending_appointment['id'],
+                                    'id' => $pending_appointment['id'],
+                                    'route_name' => '/cancel/'.$pending_appointment['id']
                                     ])
 
 
