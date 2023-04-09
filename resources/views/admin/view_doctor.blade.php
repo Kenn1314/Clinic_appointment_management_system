97371bc685382539bf1d06653881a2f2e42107fa
UtardaBest <alvinzzz@hotmail.com>
2023-04-09 18:22:51 +0800

    Update view_doctor.blade.php

--- resources/views/admin/view_doctor.blade.php
+++ resources/views/admin/view_doctor.blade.php
@@ -4,7 +4,7 @@
 
 <h1 style="text-align:center;">Doctor management page</h1>
 
-<table class="table table-striped table-hover dt-responsive nowrap" id="doctor_table" style="width:100%">
+<table class="table table-striped table-hover dt-responsive wrap" id="doctor_table" style="width:100%">
     <thead class="table-dark">
       <tr>
         <th scope="col"></th>
@@ -31,19 +31,20 @@
                         @csrf
                         <button type="submit" class="btn btn-info">UPDATE</button>
                     </form>
+                </td>
                 <td>
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target='#deleteModal_{{$doctor->id}}'>
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target='#DeleteDoctorModal_{{$doctor->id}}'>
                         DELETE
                     </button>
 
                     <!-- APPROVED MODAL DISPLAY -->
                     @include('partials.prompt_window', [
-                    'modal_id' => 'deleteModal_'.$doctor->id,
+                    'modal_id' => 'DeleteDoctorModal_'.$doctor->id,
                     'aria_label' => 'deleteModalLabel',
                     'modal_title' => 'CONFIRM TO DELETE THIS DOCTOR',
                     'modal_body' => 'NOTE: THIS DOCTOR WILL BE DELETED PERMANENTLY',
                     'id' => $doctor->id,
-                    'route_name' => "/deleteDoctor/".$doctor->id
+                    'route_name' => "deleteDoctor/".$doctor->id
                     ])
 
                 </td>
